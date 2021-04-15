347050b90c1666373c6e301d4e0eb37e72118602
Parker Knight <plb.knight@gmail.com>
2021-04-15 15:16:28 -0400

    Add fastGMD implementation

--- R/GMD.R
+++ R/GMD.R
@@ -87,3 +87,51 @@ GMD =function(X, H, Q, K){
   return(gmd.output)
 
 }
+
+
+#' Fast GMD Implementation
+#'
+#' Computes the generalized matrix decomposition of X.
+#'
+#' @param X An n x p data matrix.
+#' @param H An n x n positive semi-definite similarity kernel.
+#' @param Q An p x p positive semi-definite similarity kernel.
+#' @param K a scalar specifying the dimension of GMD components (see Details).
+#' @param scale Logical, indictes whether to scale the data.
+#'
+#' @details The K-dimensional GMD of X with respect to H and Q is given by X = USV^T, where
+#' \deqn{(U, S, V) = argmin_{U,S,V} ||X - USV^T||^2_{H, Q},}
+#' subject to \eqn{U^THU = I_K, V^TQV = I_K} and \eqn{diag(S) \ge 0}. Here, for any n x p matrix A,
+#' ||A||^2_{H,Q} = tr(A^THAQ).
+#'
+#'
+#'
+#
+#' @return A list of the GMD components U, S, V, H and Q (see Details).
+#' @author Parker Knight and Yue Wang \email{ywang2310@fredhutch.org}
+#' @references Allen, G. I., L. Grosenick, and J. Taylor (2014). A generalized least-square matrix decom- position. Journal of the American Statistical Association 109(505), 145–159.
+#' @export
+fastGMD <- function(X, H, Q, K, scale= TRUE)
+{
+  if (scale)
+  {
+     eigen.Q <- eigen(Q)
+     Q <- eigen.Q$vectors %*% diag((eigen.Q$values/eigen.Q$values[1])) %*% t(eigen.Q$vectors)
+     eigen.H <- eigen(H)
+     H <- eigen.H$vectors %*% diag((eigen.H$values/eigen.H$values[1])) %*% t(eigen.H$vectors)
+     svd.X <- svd(X)
+     X <- svd.X$u %*% diag((svd.X$d/svd.X$d[1])) %*% t(svd.X$v)
+  }
+  
+  eigen.Q <- eigen(Q)
+  L.Q <- eigen.Q$vectors %*% diag(sqrt(eigen.Q$values))
+  eigen.H <- eigen(H)
+  L.H <- eigen.H$vectors %*% diag(sqrt(eigen.H$values))
+
+  X.tilde <- t(L.H) %*% X %*% L.Q
+  svd.X <- svd(X.tilde)
+  U.star <- solve(L.H) %*% svd.X$u[,1:K]
+  V.star <- solve(t(L.Q)) %*% svd.X$v[,1:K]
+  d.star <- svd.X$d[1:K]
+  return(list(U = U.star, V = V.star, D = d.star, X = X, H = H, Q = Q))
+}
